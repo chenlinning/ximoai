0294d0e6421836c60f3940f4411dcc321aceaf64
CI User <ci@example.com>
2026-05-26 20:38:21 +0800

    style: move batch account test action

--- frontend/src/components/admin/account/AccountBulkActionsBar.vue
+++ frontend/src/components/admin/account/AccountBulkActionsBar.vue
@@ -26,12 +26,12 @@
     <div class="flex gap-2">
       <template v-if="selectedIds.length > 0">
         <button @click="$emit('delete')" class="btn btn-danger btn-sm">{{ t('admin.accounts.bulkActions.delete') }}</button>
-        <button @click="$emit('batch-test')" class="btn btn-secondary btn-sm">{{ t('admin.accounts.bulkActions.batchTest') }}</button>
         <button @click="$emit('reset-status')" class="btn btn-secondary btn-sm">{{ t('admin.accounts.bulkActions.resetStatus') }}</button>
         <button @click="$emit('refresh-token')" class="btn btn-secondary btn-sm">{{ t('admin.accounts.bulkActions.refreshToken') }}</button>
         <button @click="$emit('toggle-schedulable', true)" class="btn btn-success btn-sm">{{ t('admin.accounts.bulkActions.enableScheduling') }}</button>
         <button @click="$emit('toggle-schedulable', false)" class="btn btn-warning btn-sm">{{ t('admin.accounts.bulkActions.disableScheduling') }}</button>
         <button @click="$emit('edit-selected')" class="btn btn-primary btn-sm">{{ t('admin.accounts.bulkActions.edit') }}</button>
+        <button @click="$emit('batch-test')" class="btn btn-info btn-sm">{{ t('admin.accounts.bulkActions.batchTest') }}</button>
       </template>
       <button @click="$emit('edit-filtered')" class="btn btn-primary btn-sm">
         {{ t('admin.accounts.bulkEdit.submit') }}
